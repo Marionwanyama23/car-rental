c8faacb4139249d18628ae0be58b35f05236f43e
+ <+>
2022-07-26 14:50:59 +0300

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -69,10 +69,10 @@ const Footer = () => {
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
               <h5 className="footer__link-title mb-4">Head Office</h5>
-              <p className="office__info">354 Nairoi, Kenya</p>
+              <p className="office__info">354 Nairobi, Kenya</p>
               <p className="office__info">Phone: +254718234567</p>
 
-              <p className="office__info">Email: driveapp@gmail.com</p>
+              <p className="office__info">Email: carrental@gmail.com</p>
 
               <p className="office__info">Office Time: 10am - 7pm</p>
             </div>
