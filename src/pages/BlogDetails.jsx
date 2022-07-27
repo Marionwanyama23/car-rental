5adfb2d6698cde024453ec352ba28b8f69aa6ee7
+ <+>
2022-07-27 19:41:23 +0300

    commit

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -42,75 +42,13 @@ const BlogDetails = () => {
                   </span>
                 </div>
 
-                <p className="section__description">{blog.description}</p>
-                <h6 className="ps-5 fw-normal">
-                  <blockquote className="fs-4">{blog.quote}</blockquote>
-                </h6>
-                <p className="section__description">{blog.description}</p>
+                
               </div>
 
-              <div className="comment__list mt-5">
-                <h4 className="mb-5">3 Comments</h4>
-
-                <div className="single__comment d-flex gap-3">
-                  <img src={commentImg} alt="" />
-                  <div className="comment__content">
-                    <h6 className=" fw-bold">Moses Wanyama</h6>
-                    <p className="section__description mb-0">14 July, 2022</p>
-                    <p className="section__description">
-                      Be your own driver!
-                    </p>
-
-                    <span className="replay d-flex align-items-center gap-1">
-                      <i class="ri-reply-line"></i> Replay
-                    </span>
-                  </div>
-                </div>
-
-                {/* =============== comment form ============ */}
-                <div className="leave__comment-form mt-5">
-                  <h4>Leave a Comment</h4>
-                  <p className="section__description">
-                    You must sign-in to make or comment a post
-                  </p>
-
-                  <Form>
-                    <FormGroup className=" d-flex gap-3">
-                      <Input type="text" placeholder="Full name" />
-                      <Input type="email" placeholder="Email" />
-                    </FormGroup>
-
-                    <FormGroup>
-                      <textarea
-                        rows="5"
-                        className="w-100 py-2 px-3"
-                        placeholder="Comment..."
-                      ></textarea>
-                    </FormGroup>
-
-                    <button className="btn comment__btn mt-3">
-                      Post a Comment
-                    </button>
-                  </Form>
-                </div>
-              </div>
+              
             </Col>
 
-            <Col lg="4" md="4">
-              <div className="recent__post mb-4">
-                <h5 className=" fw-bold">Recent Posts</h5>
-              </div>
-              {blogData.map((item) => (
-                <div className="recent__blog-post mb-4" key={item.id}>
-                  <div className="recent__blog-item d-flex gap-3">
-                    <img src={item.imgUrl} alt="" className="w-25 rounded-2" />
-                    <h6>
-                      <Link to={`/blogs/${item.title}`}>{blog.title}</Link>
-                    </h6>
-                  </div>
-                </div>
-              ))}
-            </Col>
+            
           </Row>
         </Container>
       </section>
