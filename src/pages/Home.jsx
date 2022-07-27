700e04cca78d13002b4cab9ec4c621a1ff780bc1
+ <+>
2022-07-27 20:25:37 +0300

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-
 import HeroSlider from "../components/UI/HeroSlider";
 import Helmet from "../components/Helmet/Helmet";
-
 import { Container, Row, Col } from "reactstrap";
 import FindCarForm from "../components/UI/FindCarForm";
-import AboutSection from "../components/UI/AboutSection";
 import ServicesList from "../components/UI/ServicesList";
 import carData from "../assets/data/carData";
 import CarItem from "../components/UI/CarItem";
@@ -17,7 +14,6 @@ import BlogList from "../components/UI/BlogList";
 const Home = () => {
   return (
     <Helmet title="Home">
-      {/* ============= hero section =========== */}
       <section className="p-0 hero__slider-section">
         <HeroSlider />
 
@@ -37,8 +33,7 @@ const Home = () => {
           </Container>
         </div>
       </section>
-      
-      <AboutSection />
+
 
       <section>
         <Container>
