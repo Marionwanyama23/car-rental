08731c22956cc692e07a444380be8aaa5703ae83
+ <+>
2022-07-27 10:08:41 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import Layout from "./components/Layout/Layout";
-import Axios from "axios";
+
 
 function App() {
   return <Layout />;
 }
 
 export default App;
+
+
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://cars17.p.rapidapi.com/generations/8FBB0EEA-3702-407F-8A28-CA99D9E076EE',
+  headers: {
+    'X-RapidAPI-Key': 'b15d9faccdmsh243170f892ee393p1bc715jsna6a5e130085f',
+    'X-RapidAPI-Host': 'cars17.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
